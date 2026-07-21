2e2a9447b6ad3626cba6b6c9dfdab3fc773775ed
Shivansh Tuteja <tuteja101@gmail.com>
2026-07-21 19:15:09 +0530

    feat: better initialisation

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 
-SDL_Window* CreateWindow(int height, int width){
-    SDL_Window* win = SDL_CreateWindow("Ray Tracing in CPP", 100, 100, width, height, SDL_WINDOW_SHOWN);
-    if (!win) {
-        std::cout << "Failed to create a window!" << SDL_GetError() << std::endl;
-    }
-    return win; 
-}
+#define WINDOW_WIDTH 810
+#define WINDOW_HEIGHT 540
 
 int main(int argc, char* argv[]){
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* win = CreateWindow(720, 1080);
+    // initialise SDL2
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0){
+        std::cout << "Error Initializing SDL :" << SDL_GetError() << std::endl;
+        return 1;
+    };
+    
+    // initialise window
+    SDL_Window* win = SDL_CreateWindow("Ray Tracing in C++", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!win) {
+        std::cout << "Error creating Window: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // create surface
+    SDL_Surface* surf = SDL_GetWindowSurface(win);
+
+    // draw rectangle
+    SDL_FillRect(surf, NULL, SDL_MapRGB(surf->format, 255, 255, 0));
+
+    // updates surface
+    SDL_UpdateWindowSurface(win);
+
+    // pause for keystroke
     std::cin.get();
+
+    // cleanup
     SDL_DestroyWindow(win);
     SDL_Quit();
+
+    // ensuring no dangling pointers left
+    win = NULL;
+    surf = NULL;
+    
     return 0;
 }
